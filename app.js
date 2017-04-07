ccaac0ac7c297dc670aab74fa393371d5a983f3a
sretundijr <sretundijr@gmail.com>
2017-04-06 21:58:56 -0600

    added check mark functionality and remove item

--- app.js
+++ app.js
@@ -14,12 +14,16 @@ function addItem(appState, item){
 //remove function, permenantly deletes the list
 //item
 function removeItem(appState, item){
-
+    for(var i = 0; i < appState.item.length; i++){
+        if(appState.item[i].toLowerCase() === item.toLowerCase()){
+            appState.items.slice(i, 1);
+        }
+    }
 }
 
 //checked item function
 function checkMark(checkedItem){
-    checkedItem.toggleClass('shopping-item-checked');
+    checkedItem.toggleClass('shopping-item__checked');
 }
 
 //page load and listeners
@@ -29,6 +33,25 @@ $(function(){
         alert("Im here");
         e.preventDefault();
         addItem(appState, $('#shopping-list-entry').val());
-        console.log(appState.item[0]);
+        // console.log(appState.item);
+    });
+
+    //toggle checked listener
+    $('.shopping-list')
+        .on('click', '.shopping-item-toggle', function(e){
+            //use closests here to move upward to parent span?
+            // console.log($(this).parent().prev());
+            checkMark($(this)
+                .parent()
+                .prev());
+    });
+    
+    //delete button listener, removes the li element and makes a 
+    //call to remove item function
+    $('.shopping-list')
+        .on('click', '.shopping-item-delete', function(e){
+            console.log($(this).parent().prev().text());
+            removeItem(appState, $(this).val);
+            $(this).closest('li').remove();
     });
 })
